cff760954f392154ebf57c0554364c51d6ef77cb
arktrovus <arktrovus@gmail.com>
2015-09-07 04:05:06 -0400

    refactor: partition conditionals

--- sorting.py
+++ sorting.py
@@ -6,30 +6,28 @@ def _partition(a, start, end):
 
     p = start
     l = p + 1
-    r = end
 
     if len(a) < 2:
         return a
 
-    while l < r:
-        while l < r and a[l] <= a[p]:
+    while l < end:
+        while l < end and a[l] <= a[p]:
             l += 1
-        while r > l and a[p] < a[r]:
-            r -= 1
+        while end > l and a[p] < a[end]:
+            end -= 1
         temp = a[l]
-        a[l] = a[r]
-        a[r] = temp
+        a[l] = a[end]
+        a[end] = temp
 
     if a[l] <= a[p]:
-        temp = a[l]
-        a[l] = a[p]
-        a[p] = temp
-        return l
+        swap = l
     else:
-        temp = a[l - 1]
-        a[l - 1] = a[p]
-        a[p] = temp
-        return l - 1
+        swap = l - 1
+    temp = a[swap]
+    a[swap] = a[p]
+    a[p] = temp
+
+    return swap
 
 def quicksort_inplace(a, l, r):
     if l == r:
@@ -37,7 +35,7 @@ def quicksort_inplace(a, l, r):
 
     pivot = _partition(a, l, r)
 
-    if pivot > l:
+    if l < pivot:
         quicksort_inplace(a, l, pivot - 1)
     if pivot < r:
         quicksort_inplace(a, pivot + 1, r)
