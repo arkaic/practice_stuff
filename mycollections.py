b7630ed154cbd786111def68964d1d9e0a5ebd40
Henry Lin <arkhenry@gmail.com>
2015-09-07 19:15:16 -0400

    feat: implemented get

--- mycollections.py
+++ mycollections.py
@@ -1,3 +1,7 @@
+import argparse, sys, unittest
+from math import floor
+from random import randrange
+
 class HashtableOA:
     """ Open addressing method """
 
@@ -8,18 +12,31 @@ class HashtableOA:
     def __init__(self):
         self.l = [None] * self._init_size
         self.count = 0
+                
+    def get(self, k):
+        hash = index = self._hashfunc(k)
+        step = 1
+        while self.l[index] is not None and self.l[index][0] != k:
+            hash += step
+            index = hash % len(self.l)
+
+        if self.l[index] is None:
+            return None
+        else:
+            return self.l[index][1]
 
     def put(self, k, v):
         hash = self._hashfunc(k)
         if self.l[hash] is None:
             self.l[hash] = (k, v)
+            self.count += 1
         else:
             # linear probing method
-            probe = self._linear_probe(hash, 1)
+            probe = self._linear_probe(k, hash, 1)
             if self.l[probe] is None:
-                self.l[probe] = (k, v)
+                self.count += 1
+            self.l[probe] = (k, v)
 
-        self.count += 1        
         if self.count > .7 * len(self.l):
             # resize
             self.count = 0
@@ -28,14 +45,17 @@ class HashtableOA:
             for x in old_l:
                 if x is not None:
                     self.put(x[0], x[1])
-                
 
-    def _linear_probe(self, hash, step):
+    def remove(self, k):
+        # TODO
+        pass
+
+    def _linear_probe(self, k, hash, step):
         probe = (hash + step) % len(self.l)
-        if self.l[probe] is None:
+        if self.l[probe] is None or self.l[probe][0] == k:
             return probe
         else:
-            return self._linear_probe(hash + 1, step)
+            return self._linear_probe(k, hash + 1, step)
 
     def _hashfunc(self, k):
         """ m <= size of array """
@@ -43,18 +63,28 @@ class HashtableOA:
         m = len(self.l)  # prime
         return floor(m * (k * a - floor(k * a)))
 
-
 ################################################################################
-import sys
-from math import floor
-from random import randrange
 
-if __name__ == '__main__':
-    k_range = 97
-    if len(sys.argv) == 2:
-        k_range = int(sys.argv[1])
+
+def main(args):
+    k_range = args.krange
     h = HashtableOA()
     for i in range(1000):
         k = randrange(k_range)
         h.put(k, i)
     print(h.l)
+
+    k,v = 752, 7521
+
+    print("Getting {} ====> {}".format(k, h.get(k)))
+
+    print("Putting {},{}".format(k, v))
+    h.put(k, v)
+
+    print("Getting {} ====> {}".format(k, h.get(k)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('krange', help='k range')
+    args = parser.parse_args()
+    main(args)
