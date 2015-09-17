520007e4616a3a3e124aa7153972bb63dc1a8d62
Henry Lin <arkhenry@gmail.com>
2015-09-16 22:24:39 -0400

    feat: reimplemented dynamic_programming.py problem. Maybe not the MOST optimal, but it uses global vars to keep track of best orders

--- topcoder/dynamic_programming.py
+++ topcoder/dynamic_programming.py
@@ -19,121 +19,100 @@ class OrderOfOperations:
           010010
           100011
 
-        Just thoughts: 
-          - knapsack problem with no weights
-          - value -> the runtime, as small as possible
-          - weight -> ? what are the constraints?
-          - Intuitively, we want every new memory access to be as minimal as
-            possible
-            per instruction
-          - Initially, every instruction will be at their maximum values. With 
-            each one taken, the rest's values will decrease.
-            - all-zero instructions will cost nothing at all times. single-1's
-              will always cost 1 or zero. 
-              Is it ever justified to choose a two-1 in order to make a single-1
-              0? That means take a value hit of 4 over taking a 2 for the two 
-              singles
-              it removes. so never
-
-          - intuititvely, best solution is to do all single instructions first. 
-
-          
-        FINAL SOLUTION: First sort all instructions from most to least. 
-        Then do the basic knapsack strategy without weights. The kicker here is
-        that with every instruction picked, the remaining list must be sorted again
-
         """
 
-        n = len(s)
-        m = len(s[0])
-        cache = [0] * m
+        cache = [0] * len(s[0])
 
         # calc initial values and create new list of 2 tuples
-        t = []
+        init_inst_vals = []
         for instruction in s:
             k = 0
             for c in instruction:
                 if c == 1:
                     k += 1
-            t.append((instruction, int(math.pow(k, 2))))
+            init_inst_vals.append((instruction, int(math.pow(k, 2))))
 
         # sort the new tuple list and run through dp()
-        sorted_t = self._sort_instructions(t, cache)
-        print("Initial sorted_t: \n{}\n\n".format(sorted_t))
+        sorted_inst_vals = self._sort_instructions(init_inst_vals, cache)
+        print("Initial sorted_inst_vals: \n{}\n\n".format(sorted_inst_vals))
 
         order = []
-        return self.dp(sorted_t, cache, m, order)
 
-    global_best_value = None
+        self.dp(sorted_inst_vals, cache, order, 0)
+        return (self.global_best_order, self.global_best_val)
 
-    def dp(self, inst_vals, cache, order):
+
+    global_best_val = None
+    global_best_order = None
+    def dp(self, inst_vals, cache, order, running_val):
         """
-        @param inst_vals = [ (string instruction, int val), ....]
-        @param order = [ (string instruction, int val), .... ] (value of inst)
+        @param inst_vals = [ (str inst, int val), .... ]
+        @param order     = [ (str inst, int val), .... ]
         """
 
-        # while sv[0][1] == 0:
-        #     order.append(sv.pop(0))
-        # if not sv:
-        #     return (order, 0)
-        # if len(sv) == 1:
-        #     inst, val = sv.pop()
-        #     order.append((inst, val))
-        #     return (order, val)
+        if self.global_best_val is not None and running_val > self.global_best_val:
+            return
 
-        # # for each instruction that has the lowest value so far
-        # lowest = sv[0][1]
-        # choices = []
-        # for inst, val in sv:
-        #     if val > lowest:
-        #         break
-        #     # clone sv, remove this inst,val from the clone, and re-sort the clone
-        #     # with a clone of the cache. The clone cache should be modified too.
-        #     clonesv = list(sv)
-        #     clonesv.remove((inst,val))
-        #     clonecache = list(cache)
-        #     clonesv = self._sort_instructions(clonesv, clonecache)
-        #     cloneorder = list(order)
-        #     cloneorder.append((inst,val))
-        #     dp_order, dp_val = self.dp(clonesv, clonecache, cloneorder)
-        #     running_val = val + dp_val
-        #     choices.append((clonesv, clonecache, dp_order, running_val))
-        # best_choices = self._best(choices)
-        # if len(best_choices) == 1:
-        #     return (best_choices[2], best_choices[3])
-        # else:
-        #     pass
+        # Automatically add in all zero-instructions
+        while inst_vals[0][1] == 0:
+            order.append(inst_vals.pop(0))
 
+        # Update global bests when all instructions added if values are lower, 
+        # or if it's first value
+        if not inst_vals:
+            if self.global_best_val is None or running_val < self.global_best_val:
+                self.global_best_val = running_val
+                self.global_best_order = order
+            return
+
+        if len(inst_vals) == 1:
+            running_val += inst_vals[0][1]
+            if self.global_best_val is None or running_val < self.global_best_val:
+                self.global_best_val = running_val
+                order.append(inst_vals.pop())
+                self.global_best_order = order
+            return
+
+        lowest = inst_vals[0][1]
+        for inst, val in inst_vals:
+            if val > lowest:
+                break
+
+            clone_instvals = list(inst_vals)
+            clone_cache = list(cache)
+            clone_order = list(order)
+
+            # Remove instruction, update clonecache, add to cloneorder
+            clone_instvals.remove((inst,val))
+            for i, c in enumerate(inst):
+                if c == '1' and clone_cache[i] != 1:
+                    clone_cache[i] = 1
+            clone_order.append((inst, val))
+            
+            print("cloneorder={}".format(clone_order))
+            print("cloneinstvalsbefore={}".format(clone_instvals))
+            print("clonecachebefore={}".format(clone_cache))
+            clone_instvals = self._sort_instructions(clone_instvals, clone_cache)
+            print("cloneinstvalsafter= {}".format(clone_instvals))
+            print("clonecachebefore={}".format(clone_cache))
+            self.dp(clone_instvals, clone_cache, clone_order, running_val + val)
 
     def _sort_instructions(self, inst_vals, cache):        
         """ @param inst_vals: [ (str instruction, int val) ]
         Ascending order. """
-        if not inst_vals: return inst_vals
+        if not inst_vals: 
+            raise Exception("shouldn't happen")
         new_instvals = []  # size of inst_vals
-        for i, tpl in enumerate(inst_vals):
-            instruction, val = tpl
+        for i, inst_val in enumerate(inst_vals):
+            instruction, val = inst_val
+            print(instruction)
             k = 0
             for i, c in enumerate(instruction):
-                if c == '1' and cache[i] != 1: 
+                if c == '1' and cache[i] != 1:
                     k += 1
             new_instvals.append((instruction, int(math.pow(k, 2))))
 
         return sorted(new_instvals, key=lambda x: x[1])
-
-    def _best(self, choices):
-        lowest = None
-        if len(choices) = 1:
-            return choices
-        for choice in choices:
-            if lowest is None:
-                lowest = choice[3]
-            elif choice[3] < lowest:
-                lowest = choice[3]
-        best_choices = []
-        for choice in choices:
-            if choice[3] == lowest:
-                best_choices.append(choice)
-        return best_choices
 
 
 if __name__ == '__main__':
@@ -142,4 +121,4 @@ if __name__ == '__main__':
          "001"]
     answer = OrderOfOperations().minTime(s)
 
-    print("\n\nanswer\n{}".format(answer))
+    print("\n\nINPUT\n{}\n\nANSWER\nOrder: {}\nValue: {}".format(s, answer[0], answer[1]))
