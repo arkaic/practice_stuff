4602cc1159f18797bb2c91ff2d72b0aaa425a167
Henry Lin <arkhenry@gmail.com>
2015-11-18 23:31:14 -0500

    test: more test cases for removing in trie trees

--- test_trietree.py
+++ test_trietree.py
@@ -24,30 +24,49 @@ class TestTrieTree(unittest.TestCase):
         self.assertTrue(self.trie.search('World'))
         self.assertTrue(self.trie.search('Wor  ld'))
 
-    def test_remove(self):
+    def test_remove1(self):
+        # case insensitive removal
         self.trie.insert('hello')
         self.trie.insert('baker')
-        self.trie.insert('WoRld')
-        self.trie.insert('worldwings')
         self.trie.remove('hello')
         self.assertFalse(self.trie.search('hello'))
         self.trie.remove('bAKER')
-
-        # case insensitive removal
         self.assertFalse(self.trie.search('bAKER'))
         self.assertFalse(self.trie.search('baker'))
 
+    def test_remove2(self):
         # assert that 'world' should not exist even though worldwings is still in
+        self.trie.insert('worldwings')
+        self.trie.insert('world')
         self.trie.remove('world')
         self.assertFalse(self.trie.search('world'))
         self.assertTrue(self.trie.search('worldwings'))
-        self.assertFalse(self.trie.search('world'))
 
+    def test_remove3(self):
+        # assert that removing the larger word will still keep the smaller word
+        self.trie.insert('worldwings')
+        self.trie.insert('world')
+        self.trie.remove('world')
+        self.assertFalse(self.trie.search('world'))
         self.trie.insert('world')
         self.assertTrue(self.trie.search('world'))
         self.trie.remove('worldwings')
         self.assertFalse(self.trie.search('worldwings'))
         self.assertTrue(self.trie.search('world'))
+
+    def test_remove_inbetween(self):
+        self.trie.insert('back')
+        self.trie.insert('backrub')
+        self.trie.insert('backrubber')
+        self.assertTrue(self.trie.search('back'))
+        self.assertTrue(self.trie.search('backrub'))
+        self.assertTrue(self.trie.search('backrubber'))
+        self.trie.remove('backrub')
+        self.assertTrue(self.trie.search('back'))
+        self.assertFalse(self.trie.search('backrub'))
+        self.assertTrue(self.trie.search('backrubber'))
+
+
 
     def test_properties(self):
         # TODO assert that all leaves should have _word ie that the words they
